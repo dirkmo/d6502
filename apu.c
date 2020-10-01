f4719b63235173f8a35cd493703cdf8f41177fd8
Dirk Moeller <moeller.dirk80@gmail.com>
2020-10-01 20:34:06 +0200

    joystick improvements

--- apu.c
+++ apu.c
@@ -3,6 +3,8 @@
 static uint8_t joy1 = 0;
 static uint8_t joy2 = 0;
 static uint8_t strobe = 0;
+static uint8_t joy1shift = 0;
+static uint8_t joy2shift = 0;
 
 void apu_report_buttonpress(button_t button, bool pressed) {
     if (pressed) {
@@ -20,6 +22,10 @@ void apu_write(uint8_t addr, uint8_t dat) {
             break;
         case 0x16: // Joypad #1
             strobe = dat & 1;
+            if (strobe == 1) {
+                joy1shift = joy1;
+                joy2shift = joy2;
+            }
             break;
         case 0x17: // Joypad #2
             break;
@@ -35,15 +41,15 @@ uint8_t apu_read(uint8_t addr) {
         case 0x15:
             break;
         case 0x16:// Joypad #1
-            val = joy1 & 1;
+            val = joy1shift & 1;
             if (strobe == 0) {
-                joy1 = joy1 >> 1;
+                joy1shift = joy1shift >> 1;
             }
             break;
         case 0x17: // Joypad #2
-            val = joy2 & 1;
+            val = joy2shift & 1;
             if (strobe == 0) {
-                joy2 = joy2 >> 1;
+                joy2shift = joy2shift >> 1;
             }
             break;
         default: ;
